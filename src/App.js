9720f71de0748e719d8b6413c2556e9107647b68
Ifeanyi <140451177+Jonadest@users.noreply.github.com>
2024-01-25 21:46:28 +0100

    Removed Booking custom Page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import { Route, Routes} from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 import Hero from "./Components/Hero/Hero";
-import BookingForm from './Components/BookingForm/BookingForm';
-import Footer from './Components/Footer/Footer';
+
+import Footer from "./Components/Footer/Footer";
 import "./App.css";
 
-
-const App = () =>{
-  return(<div className='appWrapper'>
-    
-    
-    <Routes>
-          <Route path="/" element = {<Hero/>}/>
-
-          <Route path="/booking" element = {<BookingForm/>}/>
-
-          
-
-          
-          
-          
-        </Routes>
-        <Footer/>
-   
-  </div> )
-}
+const App = () => {
+  return (
+    <div className="appWrapper">
+      <Routes>
+        <Route path="/" element={<Hero />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
