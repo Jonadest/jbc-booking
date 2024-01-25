30e99593d53af3d55f4f64056dc57883ab75d96d
Ifeanyi <140451177+Jonadest@users.noreply.github.com>
2024-01-25 22:05:01 +0100

    2nd build 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Route, Routes } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Hero from "./Components/Hero/Hero";
-
 import Footer from "./Components/Footer/Footer";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="appWrapper">
-      <Routes>
-        <Route path="/" element={<Hero />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Hero />} />
+        </Routes>
+      </Router>
       <Footer />
     </div>
   );
