64e5ebb7804a9a18ac8ef0cd6d57a1120e33cabd
Ifeanyi <140451177+Jonadest@users.noreply.github.com>
2024-02-08 21:04:28 +0100

    Added Menus and Pages 1

--- src/Components/MenuBar/Contact.jsx
+++ src/Components/MenuBar/Contact.jsx
@@ -7,13 +7,15 @@ const Contact = () => {
         <div className="imgSide"></div>
         <div className="detailsSide text-center">
           <h1>Contact Me</h1>
+          <i className="fa-solid fa-location-dot fs-4"></i>
+          <p> Newark, New Jersey.</p>
           <p>
             <a href="tel: 862 231 8040 ">
-              <i class="fa-solid fa-mobile"></i> 862 231 8040
+              <i className="fa-solid fa-mobile"></i> 862 231 8040
             </a>
           </p>
 
-          <i class="fa-solid fa-envelope"></i>
+          <i className="fa-solid fa-envelope"></i>
           <p>
             <a href="mailto:ijannyc@gmail.com">ijannyc@gmail.com</a>
           </p>
